6c7607dd9cc972280feb1ca898e0352e547b86ae
Ruan Scherer <ruan.scherer@paytrack.com.br>
2022-01-24 20:06:19 -0300

    feat: update home screen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,3 +1,7 @@
+import 'package:divide_ai/enums/transaction_type_enum.dart';
+import 'package:divide_ai/screens/new_bill_screen.dart';
+import 'package:divide_ai/widgets/status_card.dart';
+import 'package:divide_ai/widgets/transaction_tile.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -9,261 +13,162 @@ class HomeScreen extends StatelessWidget {
       backgroundColor: Theme.of(context).backgroundColor,
       body: SafeArea(
         child: SingleChildScrollView(
-          child: Container(
-            padding: const EdgeInsets.all(22),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: [
-                const Text(
-                  'Boa tarde,',
-                  style: TextStyle(
-                    fontSize: 18,
-                  ),
+          padding: const EdgeInsets.all(22),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              const Text(
+                'Boa tarde,',
+                style: TextStyle(
+                  fontSize: 18,
                 ),
-                const Text(
-                  'Ruan',
-                  style: TextStyle(
-                    fontSize: 24,
-                    fontWeight: FontWeight.bold,
-                    height: 1,
-                  ),
+              ),
+              const Text(
+                'Ruan',
+                style: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                  height: 1,
                 ),
-                const SizedBox(
-                  height: 20,
-                ),
-                Row(
-                  children: [
-                    Expanded(
-                      flex: 1,
-                      child: Card(
-                        elevation: 0,
-                        child: Container(
-                          padding: const EdgeInsets.symmetric(
-                              horizontal: 15, vertical: 30),
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(18),
-                            color: const Color(0xFF00E4A7),
-                          ),
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.stretch,
-                            children: [
-                              const Align(
-                                alignment: Alignment.centerLeft,
-                                child: Icon(
-                                  Icons.arrow_circle_up_rounded,
-                                  size: 40,
-                                ),
-                              ),
-                              const SizedBox(
-                                height: 12,
-                              ),
-                              const Text(
-                                'A receber',
-                                style: TextStyle(
-                                  fontSize: 16,
-                                  color: Colors.black,
-                                  fontWeight: FontWeight.bold,
-                                ),
-                              ),
-                              Row(
-                                children: [
-                                  const Text(
-                                    'R\$',
-                                    style: TextStyle(fontWeight: FontWeight.bold),
-                                  ),
-                                  Expanded(
-                                    child: Text(
-                                      '9.999,00',
-                                      style: TextStyle(
-                                        fontSize: 22,
-                                        color: Colors.black,
-                                        fontWeight: FontWeight.bold,
-                                      ),
-                                    ),
-                                  ),
-                                ],
-                              ),
-                            ],
-                          ),
-                        ),
-                      ),
-                    ),
-                    const SizedBox(
-                      width: 2,
-                    ),
-                    Expanded(
-                      flex: 1,
-                      child: Card(
-                        elevation: 0,
-                        child: Container(
-                          padding: const EdgeInsets.symmetric(
-                              horizontal: 15, vertical: 30),
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(18),
-                            color: const Color(0xFFFF1865),
-                          ),
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.stretch,
-                            children: [
-                              const Align(
-                                alignment: Alignment.centerLeft,
-                                child: Icon(
-                                  Icons.arrow_circle_down_rounded,
-                                  size: 40,
-                                ),
-                              ),
-                              const SizedBox(
-                                height: 12,
-                              ),
-                              const Text(
-                                'A pagar',
-                                style: TextStyle(
-                                  fontSize: 16,
-                                  color: Colors.black,
-                                  fontWeight: FontWeight.bold,
-                                ),
-                              ),
-                              Row(
-                                children: [
-                                  const Text(
-                                    'R\$',
-                                    style: TextStyle(fontWeight: FontWeight.bold),
-                                  ),
-                                  Expanded(
-                                    child: Text(
-                                      '9.999,00',
-                                      style: TextStyle(
-                                        fontSize: 22,
-                                        color: Colors.black,
-                                        fontWeight: FontWeight.bold,
-                                      ),
-                                    ),
-                                  ),
-                                ],
-                              ),
-                            ],
-                          ),
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-                const SizedBox(
-                  height: 10,
-                ),
-                TextButton(
-                  onPressed: () {},
-                  style: ButtonStyle(
-                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                      RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(14),
-                      ),
-                    ),
-                    backgroundColor:
-                        MaterialStateProperty.all<Color>(Colors.grey[200]!),
-                    overlayColor:
-                        MaterialStateProperty.all<Color>(Colors.grey[300]!),
-                  ),
-                  child: Container(
-                    padding: const EdgeInsets.all(4),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      crossAxisAlignment: CrossAxisAlignment.end,
-                      children: const [
-                        Icon(
-                          Icons.add,
-                          color: Colors.black,
-                        ),
-                        Text(
-                          'Dividir nova conta',
-                          style: TextStyle(
-                            fontSize: 16,
-                            color: Colors.black,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                      ],
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Row(
+                children: const [
+                  Expanded(
+                    flex: 1,
+                    child: StatusCard(
+                      transactionType: TransactionType.income,
+                      amount: 999.99,
+                      label: 'A receber',
                     ),
                   ),
-                ),
-                const SizedBox(
-                  height: 26,
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    const Text(
-                      'Últimas movimentações',
-                      style: TextStyle(
-                        fontSize: 20,
-                        fontWeight: FontWeight.bold,
-                      ),
+                  SizedBox(
+                    width: 2,
+                  ),
+                  Expanded(
+                    flex: 1,
+                    child: StatusCard(
+                      transactionType: TransactionType.spent,
+                      amount: 57.5,
+                      label: 'A pagar',
                     ),
-                    IconButton(
-                      padding: EdgeInsets.zero,
-                      constraints: const BoxConstraints(),
-                      icon: const Icon(
-                        Icons.chevron_right_rounded,
-                        size: 30,
-                      ),
-                      onPressed: () {},
+                  ),
+                ],
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              TextButton(
+                onPressed: () {
+                  Navigator.of(context).push(
+                    MaterialPageRoute(
+                      builder: (context) => const NewBillScreen(),
                     ),
-                  ],
+                  );
+                },
+                style: ButtonStyle(
+                  shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                    RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(14),
+                    ),
+                  ),
+                  backgroundColor:
+                      MaterialStateProperty.all<Color>(Colors.grey[200]!),
+                  overlayColor:
+                      MaterialStateProperty.all<Color>(Colors.grey[300]!),
                 ),
-                const SizedBox(
-                  height: 10,
-                ),
-                Column(
-                  children: [
-                    ListTile(
-                      contentPadding: const EdgeInsets.symmetric(
-                        horizontal: 0,
-                        vertical: 4,
+                child: Container(
+                  padding: const EdgeInsets.all(4),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    children: const [
+                      Icon(
+                        Icons.add,
+                        color: Colors.black,
                       ),
-                      leading: Container(
-                        padding: const EdgeInsets.all(5),
-                        decoration: BoxDecoration(
-                          color: Colors.grey[100],
-                          borderRadius: BorderRadius.circular(12),
-                        ),
-                        child: Icon(
-                          Icons.arrow_circle_up_rounded,
-                          size: 32,
-                          color: Color(0xFF00E4A7),
-                        ),
-                      ),
-                      title: Column(
-                        crossAxisAlignment: CrossAxisAlignment.stretch,
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Text(
-                            'Rolê de domingo',
-                            style: TextStyle(
-                              fontSize: 16,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                          Text(
-                            '01 de Março',
-                            style: TextStyle(
-                              fontSize: 14,
-                              color: Colors.grey[600],
-                            ),
-                          ),
-                        ],
-                      ),
-                      trailing: Text(
-                        'R\$59,90',
+                      Text(
+                        'Dividir nova conta',
                         style: TextStyle(
-                          fontSize: 14,
+                          fontSize: 16,
+                          color: Colors.black,
                           fontWeight: FontWeight.bold,
                         ),
                       ),
-                    )
-                  ],
+                    ],
+                  ),
                 ),
-              ],
-            ),
+              ),
+              const SizedBox(
+                height: 26,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  const Text(
+                    'Últimas movimentações',
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  IconButton(
+                    padding: EdgeInsets.zero,
+                    constraints: const BoxConstraints(),
+                    icon: const Icon(
+                      Icons.chevron_right_rounded,
+                      size: 30,
+                    ),
+                    onPressed: () {},
+                  ),
+                ],
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              Column(
+                children: [
+                  TransactionTile(
+                    transactionType: TransactionType.income,
+                    transactionName: 'Rolê de domingo',
+                    transactionDate: DateTime.now(),
+                    transactionAmount: 59.9,
+                  ),
+                  TransactionTile(
+                    transactionType: TransactionType.income,
+                    transactionName: 'Fut de domingo',
+                    transactionDate: DateTime.now(),
+                    transactionAmount: 11.5,
+                  ),
+                  TransactionTile(
+                    transactionType: TransactionType.spent,
+                    transactionName: 'Churrasco do vini',
+                    transactionDate: DateTime.now(),
+                    transactionAmount: 25.7,
+                  ),
+                  TransactionTile(
+                    transactionType: TransactionType.income,
+                    transactionName: 'Férias na praia',
+                    transactionDate: DateTime.now(),
+                    transactionAmount: 78,
+                  ),
+                  TransactionTile(
+                    transactionType: TransactionType.spent,
+                    transactionName: 'Fut de domingo',
+                    transactionDate: DateTime.now(),
+                    transactionAmount: 10,
+                  ),
+                  TransactionTile(
+                    transactionType: TransactionType.spent,
+                    transactionName: 'Pizzaria',
+                    transactionDate: DateTime.now(),
+                    transactionAmount: 40.5,
+                  ),
+                ],
+              ),
+            ],
           ),
         ),
       ),
